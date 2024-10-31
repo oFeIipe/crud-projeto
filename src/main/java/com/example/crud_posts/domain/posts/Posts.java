677eb1aaf4b04b65b15bb3a7180fea9a0038883c
ofeiipe <costadecarvalho.felipe@gmail.com>
2024-10-30 23:31:48 -0300

    Delete e classe exception

--- src/main/java/com/example/crud_posts/domain/posts/Posts.java
+++ src/main/java/com/example/crud_posts/domain/posts/Posts.java
@@ -1,6 +1,5 @@
 package com.example.crud_posts.domain.posts;
 
-
 import jakarta.persistence.*;
 import lombok.*;
 
---
+++ src/main/java/com/example/crud_posts/infra/RequestExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.example.crud_posts.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RequestExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(){
+        return ResponseEntity.badRequest().body("Post não encontrado");
+    }
+}
